24096b5cca8b71eb06592d199f0d609bd5dc34ff
Ubuntu <ubuntu@ip-172-31-47-161.ap-southeast-7.compute.internal>
2025-08-03 03:09:05 +0000

    Initial commit for Final Exam project

---
+++ html/about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About Me</title>
+</head>
+<body style="background-color:black; color:white; text-align:center;">
+  <h1>About</h1>
+  <img src="cjcj.jpg" width="300" style="border-radius: 12px;"><br><br>
+
+  <p>ชื่อ: นายพัฒนวัตร เรียงวงษ์</p>
+  <p>รหัสนักศึกษา: 67130141</p>
+  <p>งานอดิเรก: ถ่ายภาพ, ฟังเพลง, เดินป่า</p>
+  <p>อีเมล: 67130141@dpu.ac.th</p>
+
+  <a href="/index.php" style="color:lightblue;">← กลับหน้าแรก</a>
+</body>
+</html>
---
+++ html/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Final Exam</title>
+</head>
+<body style="background-color:black; color:white; text-align:center;">
+  <h1>ระบบตรวจจับ Phishing</h1>
+
+  <ul style="list-style-type:none;">
+    <li><a href="/about.php">About</a></li>
+    <li><a href="/myresearch.php">My Research</a></li>
+    <li><a href="/reference.php">References</a></li>
+  </ul>
+
+  <br>
+
+  <img src="cjcj.jpg" width="300" style="border-radius: 12px;"><br><br>
+
+  <p>ชื่อ: นายพัฒนวัตร เรียงวงษ์</p>
+  <p>รหัสนักศึกษา: 67130141</p>
+
+  <p><a href="https://github.com/yourusername/FinalExamPhishingDetection">GitHub Source Code</a></p>
+</body>
+</html>
+
+
